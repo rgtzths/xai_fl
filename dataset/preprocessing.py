d1cfdabae0da28312c0f858796ede7a7c53259de
Rafael Teixeira <rafaelgteixeira@ua.pt>
2023-09-15 20:57:12 +0100

    Adding finished preprocessing

--- dataset/preprocessing.py
+++ dataset/preprocessing.py
@@ -2,17 +2,51 @@ import pandas as pd
 from sklearn.preprocessing import OneHotEncoder
 import numpy as np
 import pathlib
+import gc
+
+def finding_closest_stations(rl_sites_file, distances_file, met_stations_file, k=1):
+    rl_sites_df = pd.read_csv(rl_sites_file, sep="\t", index_col=0)
+    distances_df = pd.read_csv(distances_file, sep="\t", index_col=0)
+    met_stations_df = pd.read_csv(met_stations_file, sep="\t", index_col=0)
+
+    rl_stations = rl_sites_df["site_id"].unique()
+    met_stations = met_stations_df["station_no"].unique()
+
+    distances_df = distances_df.loc[met_stations, rl_stations]
+
+    closest_stations = dict(distances_df.idxmin())
+
+    return closest_stations
 
 
-def preprocessing(kpi_file, distances_file, met_forecast_file, met_real_file, output_path, prediction_interval=5, look_back_interval=10):
+
+def preprocessing(kpi_file, distances_file, met_forecast_file, rl_sites_file, met_stations_file, output_path, prediction_interval=5, look_back_interval=10):
     output = pathlib.Path(output_path)
     output.mkdir(parents=True, exist_ok=True)
+
+    closest_stations = finding_closest_stations(rl_sites_file, distances_file, met_stations_file)
 
     identifiers = ["site_id", "mlid", "datetime"]
     df = pd.read_csv(kpi_file, sep="\t", index_col=0)
 
     if "datetime" in df:
         df["datetime"] = pd.to_datetime(df["datetime"])
+
+    met_forecast_df = pd.read_csv(met_forecast_file, sep="\t", index_col=0)
+    if "datetime" in met_forecast_df:
+        met_forecast_df["datetime"] = pd.to_datetime(met_forecast_df["datetime"])
+    
+    met_forecast_df = met_forecast_df[ met_forecast_df["report_time"] == "morning"]
+    met_forecast_df.drop(columns=["report_time"], inplace=True)
+
+    df["station_no"] = [closest_stations[site_id] for  site_id in df["site_id"]]
+
+    df = df.merge(met_forecast_df, how="left", on=["datetime", "station_no"])
+    
+    del met_forecast_df
+    del closest_stations
+
+    df.drop(columns=["station_no", "mw_connection_no"], inplace=True)
 
     df_labels = df.loc[:, identifiers]
 
@@ -42,63 +76,87 @@ def preprocessing(kpi_file, distances_file, met_forecast_file, met_real_file, ou
     rl_kpis_with_labels = df.merge(df_labels, 
                                     how="left", 
                                     on=["datetime", "site_id", "mlid"])
-    
-    categorical_features = ["card_type", "freq_band", "type", "tip", "adaptive_modulation", "freq_band", "modulation"]
+    del df_labels
 
-    numerical_features = ["severaly_error_second", "error_second", "unavail_second", "avail_time", "bbe", "rxlevmax", "capacity"]
-
+    static_features = ["card_type", "freq_band", "type", "tip", "adaptive_modulation", "freq_band", "modulation"]
     labels = ["rlf", "1-day-predict", "5-day-predict"]
+    time_sentitive_features = [feature for feature in df.columns if feature not in static_features and feature not in labels and feature not in identifiers]
 
-    rl_kpis_with_labels = rl_kpis_with_labels.dropna()
-
-    rl_kpis_with_labels[labels] = rl_kpis_with_labels[labels].astype(int)
-
+    #Check for nans in static_features
     one_hot_encoder = OneHotEncoder(handle_unknown='error')
-    one_hot_encoder.fit(rl_kpis_with_labels[categorical_features])
-    rl_kpis_with_labels[one_hot_encoder.get_feature_names_out()] = one_hot_encoder.transform(rl_kpis_with_labels[categorical_features]).toarray()
+    one_hot_encoder.fit(rl_kpis_with_labels[static_features])
+    rl_kpis_with_labels[one_hot_encoder.get_feature_names_out()] = one_hot_encoder.transform(rl_kpis_with_labels[static_features]).toarray()
 
-    rl_kpis_with_labels = rl_kpis_with_labels.drop(columns=categorical_features)
-    categorical_features = list(one_hot_encoder.get_feature_names_out())
+    rl_kpis_with_labels.drop(columns=static_features, inplace=True)
+    static_features = list(one_hot_encoder.get_feature_names_out())
 
-    numerical_dataset = rl_kpis_with_labels.loc[:, identifiers + numerical_features + labels]
+    time_sentitive_dataset = rl_kpis_with_labels.loc[:, identifiers + time_sentitive_features + labels]
 
-    categorical_dataset = rl_kpis_with_labels.loc[:, identifiers + categorical_features + labels]
+    static_dataset = rl_kpis_with_labels.loc[:, identifiers + static_features + labels]
+    
+    del rl_kpis_with_labels
 
-    features = numerical_features.copy()
-
-    for feature in numerical_features:
-        historical_num_dataset = numerical_dataset.loc[:, identifiers]
+    for feature in time_sentitive_features:
+        historical_sen_dataset = time_sentitive_dataset.loc[:, identifiers]
         for i in range(-1,  -look_back_interval, -1):
-            historical_num_dataset[f"T{i}"] = historical_num_dataset["datetime"] + pd.DateOffset(days=i)
+            historical_sen_dataset[f"T{i}"] = historical_sen_dataset["datetime"] + pd.DateOffset(days=i)
 
-        numeric_view = numerical_dataset[identifiers + [feature]]
+        feature_view = time_sentitive_dataset[identifiers + [feature]]
         for i in range(-1,  -look_back_interval, -1):
             target_day_column_name = f"T{i}"
 
-            historical_num_dataset = historical_num_dataset.merge(numeric_view, 
+            historical_sen_dataset = historical_sen_dataset.merge(feature_view, 
                     how = "left", 
                     left_on = ("site_id", "mlid", target_day_column_name),
                     right_on = identifiers,
                     suffixes = ("", "_y")
             )
-            historical_num_dataset.rename(columns={ feature: f"{target_day_column_name}_{feature}"}, inplace=True)
-            features.append(f"{target_day_column_name}_{feature}")
+            historical_sen_dataset.rename(columns={ feature: f"{target_day_column_name}_{feature}"}, inplace=True)
+        
+        del feature_view
 
-        historical_num_dataset.drop(columns=["datetime_y"], inplace=True)
+        historical_sen_dataset.drop(columns=["datetime_y"], inplace=True)
 
-        historical_num_dataset = historical_num_dataset.drop(columns=[f"T{i}" for i in range(-1,  -look_back_interval, -1)])
+        historical_sen_dataset.drop(columns=[f"T{i}" for i in range(-1,  -look_back_interval, -1)], inplace=True)
 
-        numerical_dataset = numerical_dataset.merge(historical_num_dataset, 
+        time_sentitive_dataset = time_sentitive_dataset.merge(historical_sen_dataset, 
                     how="left", 
                     on=["datetime", "site_id", "mlid"])
         
-    numerical_dataset = numerical_dataset.dropna()
-    numerical_dataset = numerical_dataset[identifiers + features + labels]
+        del historical_sen_dataset
+        gc.collect()
 
-    numerical_dataset.to_csv(output/"preprocessed_numerical_features.csv", index=None)
-    categorical_dataset.to_csv(output/"preprocessed_categorical_features.csv", index=None)
+    features = time_sentitive_features.copy()
+    for i in range(-1,  -look_back_interval, -1):
+        for feature in time_sentitive_features:
+            features.append(f"T{i}_{feature}")
 
-    print(numerical_dataset.columns)
-    print(numerical_dataset.head())
+    time_sentitive_dataset = time_sentitive_dataset.dropna()
+    time_sentitive_dataset[labels] = time_sentitive_dataset[labels].astype(int)
 
-preprocessing("train/rl-kpis.tsv", "train/distances.tsv", "met-forecast.tsv", "met-real.tsv", ".")
+    weather_features = []
+
+    for j in range(0, -look_back_interval, -1):
+        for i in range(1, 6, 1):
+            if j < 0:
+                weather_features.append(f"T{j}_weather_day{i}")
+            else:
+                weather_features.append(f"weather_day{i}")
+
+
+    one_hot_encoder.fit(time_sentitive_dataset[weather_features])
+    time_sentitive_dataset[one_hot_encoder.get_feature_names_out()] = one_hot_encoder.transform(time_sentitive_dataset[weather_features]).toarray()
+
+    time_sentitive_dataset.drop(columns=weather_features, inplace=True)
+    time_sentitive_features = [feature for feature in time_sentitive_dataset.columns if feature not in labels and feature not in identifiers]
+
+    time_sentitive_dataset = time_sentitive_dataset[identifiers + time_sentitive_features + labels]
+
+    time_sentitive_dataset.to_csv(output/"preprocessed_time_sentitive_features.csv", index=None)
+    static_dataset.to_csv(output/"preprocessed_static_features.csv", index=None)
+
+    print(time_sentitive_dataset.columns)
+    print(time_sentitive_dataset.head())
+
+preprocessing("train/rl-kpis.tsv", "train/distances.tsv", "train/met-forecast.tsv", "train/rl-sites.tsv",  "train/met-stations.tsv",".")
+
