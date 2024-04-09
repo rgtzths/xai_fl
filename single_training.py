229520118e74d31986902282f138768e22731c8f
Leonardo Almeida <leonardoalmeida7777@gmail.com>
2024-04-09 17:09:56 +0100

    update single training

--- single_training.py
+++ single_training.py
@@ -1,3 +1,7 @@
+# import os
+# os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+# os.environ['CUDA_VISIBLE_DEVICES'] = '-1'
+
 import argparse
 import tensorflow as tf
 import numpy as np
@@ -11,7 +15,7 @@ from config import DATASETS, OPTIMIZERS
 tf.keras.utils.set_random_seed(42)
 
 class CustomCallback(tf.keras.callbacks.Callback):
-    def __init__(self, X_val,  y_val, start_time, verbose=0, checkpoint_file="model_checkpoint.keras", threshold=1):
+    def __init__(self, X_val,  y_val, start_time, verbose=0, checkpoint_file="model_checkpoint.keras", threshold=1, patience=10, min_delta=0.001):
         super().__init__()
         self.X_val = X_val
         self.y_val = y_val
@@ -21,6 +25,8 @@ class CustomCallback(tf.keras.callbacks.Callback):
         self.best_mcc = -1
         self.checkpoint_file = checkpoint_file
         self.threshold = threshold
+        self.patience_buffer = [0]*patience
+        self.min_delta = min_delta
 
     def on_train_begin(self, logs={}):
         self.val_f1s = []
@@ -42,11 +48,19 @@ class CustomCallback(tf.keras.callbacks.Callback):
         self.val_f1s.append(val_f1)
         self.val_mccs.append(val_mcc)
         self.times.append(time.time() - self.start_time)
+        self.patience_buffer = self.patience_buffer[1:]
+        self.patience_buffer.append(val_mcc)
 
         if self.verbose > 0:
             print("\n- val_f1: %f - val_mcc %f" %(val_f1, val_mcc))
 
-        if val_mcc > self.threshold:
+        p_stop = True
+        max_mcc = max(self.val_mccs[:-len(self.patience_buffer)], default=0)
+        max_buffer = max(self.patience_buffer, default=0)
+        if max_mcc + self.min_delta <= max_buffer:
+            p_stop = False
+
+        if val_mcc > self.threshold or p_stop:
             self.model.stop_training = True
 
     def get_metrics(self):
@@ -57,8 +71,10 @@ parser = argparse.ArgumentParser()
 parser.add_argument("-d", help=f"Dataset name {list(DATASETS.keys())}", default="IOT_DNL")
 parser.add_argument("-o", help=f"Optimizer {list(OPTIMIZERS.keys())}", default="Adam")
 parser.add_argument("-s", help="MCC score to achieve", default=1, type=float)
+parser.add_argument("-p", help="Patience", default=5, type=int)
+parser.add_argument("-md", help="Minimum Delta", default=0.001, type=float)
 parser.add_argument("-lr", help="Learning rate", default=0.001, type=float)
-parser.add_argument("-e", help="Number of epochs", default=200, type=int)
+parser.add_argument("-e", help="Number of epochs", default=100, type=int)
 parser.add_argument("-b", help="Batch size", default=1024, type=int)
 args = parser.parse_args()
 
@@ -91,7 +107,7 @@ model.compile(
 
 start = time.time()
 
-custom_metric = CustomCallback(x_val, y_val, start, 2, output/"checkpoint_file.keras", args.s)
+custom_metric = CustomCallback(x_val, y_val, start, 2, output/"checkpoint_file.keras", args.s, args.p, args.md)
 
 history = model.fit(
     x_train, 
@@ -103,6 +119,7 @@ history = model.fit(
 )
 
 end = time.time()
+print(f"Training time: {end - start} seconds")
 
 metrics = custom_metric.get_metrics()
 
@@ -117,6 +134,7 @@ f.write(history)
 f.close()
 
 model.load_weights(output/"checkpoint_file.keras")
+model.save(output/"single_training.keras")
 
 
 for type_, x_, y_ in (
@@ -131,7 +149,5 @@ for type_, x_, y_ in (
     print(f"Accuracy: {accuracy_score(y_, y_pred)}")
     print(f"F1 score: {f1_score(y_, y_pred, average='macro')}")
     print(f"MCC: {matthews_corrcoef(y_, y_pred)}")
-
-model.save(f"{args.d}/models/single_training.keras")
 
 print(f"\n\nTraining time: {end - start} seconds")
